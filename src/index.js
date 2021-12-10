86da6f3343e4a1b94f1e0c41c909384b581f51c4
Chandra <Chandrabachtiar25@gmail.com>
2021-12-10 16:54:01 +0700

    Next Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// import Lifecycle from './react-js-dasar/Lifecycle';
+// import Map from './react-js-dasar/Map';
+// import App from './react-js-dasar/App';
+// import Header from './react-js-dasar/Header';
+// import StateProps from './react-js-dasar/StateProps'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Crud from './crud';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Crud />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
